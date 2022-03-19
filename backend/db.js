f6cbf7e7fb1da7220ea97d7ed8f0a625d1692297
Asadullah Baig <61774598+Asadullah-Baig@users.noreply.github.com>
2022-03-19 18:43:39 +0500

    inotebook app

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+  "mongodb+srv://asad:asad123@cluster0.2cwuq.mongodb.net/inotebook?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
